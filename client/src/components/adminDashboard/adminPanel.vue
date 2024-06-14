5073e98775648b06d4d586a2776788bb8116cf56
MoussaST <moussa21sr@gmail.com>
2024-06-15 00:51:56 +0200

    Ajout du composant Widget au Dashboard Admin

--- client/src/components/adminDashboard/adminPanel.vue
+++ client/src/components/adminDashboard/adminPanel.vue
@@ -1,23 +1,33 @@
 <template>
-    <div class="container mx-auto px-4 py-8">
-      <ResourcesCrud />
-      <Dashboard />
+  <div class="container mx-auto px-4 py-8">
+    <!-- Section CRUD des ressources -->
+    <ResourcesCrud />
+    <Dashboard />
+    
+    <!-- Section du tableau de bord avec widgets -->
+    <div class="bg-white shadow-md rounded-lg overflow-hidden mt-8">
+      <div class="px-6 py-4">
+        <h2 class="text-xl font-semibold text-gray-800 mb-4">Widget</h2>
+        <WidgetContainer />
+      </div>
     </div>
-  </template>
-  
-  <script>
-  import ResourcesCrud from './panelComponents/resourcesCrud.vue';
-  import Dashboard from './panelComponents/dashboard.vue';
-  
-  export default {
-    name: 'AdminPanel',
-    components: {
-      ResourcesCrud,
-      Dashboard,
-    },
-  };
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  </div>
+</template>
+
+<script>
+import Dashboard from './panelComponents/dashboard.vue';
+import ResourcesCrud from './panelComponents/resourcesCrud.vue';
+import WidgetContainer from './widgetContainer.vue';
+
+export default {
+  name: 'AdminPanel',
+  components: {
+    Dashboard,
+    ResourcesCrud,
+    WidgetContainer,
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ client/src/components/adminDashboard/widgetContainer.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+      <!-- Bouton pour ajouter un nouveau widget -->
+      <button @click="addNewWidget" class="bg-blue-500 text-white hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 px-4 py-2 rounded-md mb-4 mr-2">
+        Ajouter un Widget
+      </button>
+      <button @click="removeWidget" class="bg-red-500 text-white rounded hover:text-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50 px-4 py-2 mb-4">
+        Supprimer un Widget
+      </button>
+  
+      <!-- Liste des widgets -->
+      <div v-for="(widget, index) in widgets" :key="index" class="mb-4">
+        <component :is="widget.component" @remove="removeWidget(index)" />
+      </div>
+    </div>
+</template>
+  
+<script>
+  import SalesWidget from './widgetComponents/salesWidget.vue';
+  import TopProductsWidget from './widgetComponents/topProductsWidget.vue';
+  import LatestOrdersWidget from './widgetComponents/latestOrdersWidget.vue';
+  
+  export default {
+    components: {
+      SalesWidget,
+      TopProductsWidget,
+      LatestOrdersWidget,
+    },
+    data() {
+      return {
+        widgets: [], // Tableau des widgets affichés
+        availableWidgets: [ // Liste des widgets disponibles
+          { name: 'Ventes Mensuelles', component: 'SalesWidget' },
+          { name: 'Produits les plus vendus', component: 'TopProductsWidget' },
+          { name: 'Dernières Commandes', component: 'LatestOrdersWidget' },
+        ],
+      };
+    },
+    methods: {
+      addNewWidget() {
+        // Sélectionnez un widget au hasard à ajouter (ou mettez en œuvre la logique de sélection souhaitée)
+        const randomIndex = Math.floor(Math.random() * this.availableWidgets.length);
+        const selectedWidget = this.availableWidgets[randomIndex];
+  
+        // Ajoutez le widget sélectionné à la liste des widgets
+        this.widgets.push(selectedWidget);
+      },
+      removeWidget(index) {
+        // Supprimez le widget à l'index spécifié de la liste des widgets
+        this.widgets.splice(index, 1);
+      },
+    },
+  };
+</script>
+  
+<style>
+</style>
+  
