32758950a91c27277c8b3f33cd2f1428085fda50
iSaaMz <sami.assiakh@gmail.com>
2024-06-18 12:49:58 +0200

    traductions

--- client/src/components/loginRegister/resetPassword/formResetPassword.vue
+++ client/src/components/loginRegister/resetPassword/formResetPassword.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="container flex items-center justify-center my-20">
         <div class="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-            <h2 class="text-3xl font-semibold text-center text-gray-900">Reset Password</h2>
+            <h2 class="text-3xl font-semibold text-center text-gray-900">Reinitialisez votre mot de passe</h2>
             <form class="mt-8 space-y-6" @submit.prevent="handleSubmit">
                 <div class="space-y-4">
                     <div>
                         <label for="email" class="sr-only">Email</label>
                         <input v-model="email" id="email" name="email" type="email" required
                             class="relative block w-full px-3 py-3 text-[14px] bg-gray-100 text-gray-900 placeholder-gray-500 border border-gray-300  focus:outline-none sm:text-sm"
-                            placeholder="Enter your email" />
+                            placeholder="Entrez votre email" />
                     </div>
                 </div>
                 <div>
                     <button type="submit"
                         class="relative flex justify-center px-4 py-2 text-md font-medium text-white bg-red-600 border border-transparent group hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-                        Reset Password
+                        Envoyer
                     </button>
                 </div>
                 <div v-if="success" class="text-green-500">{{ success }}</div>
