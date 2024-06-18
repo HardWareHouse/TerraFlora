d2ed8540bbd164a73cede7c3fef7b2c80cd3d561
Jay Bury <buryj15@gmail.com>
2024-06-18 15:01:34 +0200

    changer vers le francais

--- client/src/components/contact/ContactInfo.vue
+++ client/src/components/contact/ContactInfo.vue
@@ -2,7 +2,7 @@
   <div class="flex justify-center min-h-screen">
     <div class="w-2/3">
       <div class="mt-5">
-        <h2 class="text-2xl text-center font-bold mb-6">Contact Us</h2>
+        <h2 class="text-2xl text-center font-bold mb-6">Contacter Nous</h2>
         <form
           id="contact-form"
           action="https://whizthemes.com/mail-php/genger/mail.php"
@@ -14,7 +14,7 @@
             v-model="form.name"
             name="name"
             type="text"
-            placeholder="Name *"
+            placeholder="Nom *"
             required
             class="p-2 bg-gray-200 border-b-2 border-transparent focus:outline-none focus:border-gray-400"
           />
@@ -22,7 +22,7 @@
             v-model="form.email_address"
             name="email_address"
             type="text"
-            placeholder="Email *"
+            placeholder="Adresse mail *"
             required
             class="p-2 bg-gray-200 border-b-2 border-transparent focus:outline-none focus:border-gray-400"
           />
@@ -32,17 +32,17 @@
             required
             class="p-2 bg-gray-200 border-b-2 border-transparent focus:outline-none focus:border-gray-400"
           >
-            <option disabled value="">Select a reason *</option>
-            <option value="order_inquiry">Order Inquiry</option>
-            <option value="shipping_info">Shipping Information</option>
-            <option value="product_info">Product Information</option>
-            <option value="gdpr">Exercise of Data Privacy Rights (GDPR)</option>
-            <option value="other">Other</option>
+            <option disabled value="">Sélectionnez une raison *</option>
+            <option value="order_inquiry">Informations sur une commande</option>
+            <option value="shipping_info">Informations sur l'expédition</option>
+            <option value="product_info">Informations sur un produit</option>
+            <option value="gdpr">Demandes d'exercices de droits (RGPD)</option>
+            <option value="other">Autre</option>
           </select>
           <textarea
             v-model="form.message"
             name="message"
-            placeholder="Message *"
+            placeholder="Votre message *"
             required
             class="p-2 bg-gray-200 border-b-2 border-transparent focus:outline-none focus:border-gray-400"
           ></textarea>
@@ -52,7 +52,7 @@
                 type="submit"
                 class="btn bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
               >
-                Send Message
+                Envoyer
               </button>
             </div>
             <div class="col-span-1 flex justify-center">
