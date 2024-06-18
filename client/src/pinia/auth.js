f406089f5ce4a4967b05182d965d4cb04bcbb067
AriaAman <ariaaman@outlook.fr>
2024-06-18 14:10:44 +0200

    Clean header, check token onMounted

--- client/src/pinia/auth.js
+++ client/src/pinia/auth.js
@@ -1,62 +1,79 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'http://localhost:8000/'
+});
+
 export const useAuthStore = defineStore('auth', {
-    state: () => ({
-        token: null,
-        nom: "",
-        id: "",
-        prenom: "",
-        email: "",
-        role: "",
-        wantsMailChangingPrice: null,
-        wantsMailNewProduct: null,
-        wantsMailNewsletter: null,
-        wantsMailRestockProduct: null,  
-        error: "",
-        success: "",
-    }),
-    actions: {
+  state: () => ({
+    token: null,
+    nom: "",
+    id: "",
+    prenom: "",
+    email: "",
+    role: "",
+    wantsMailChangingPrice: null,
+    wantsMailNewProduct: null,
+    wantsMailNewsletter: null,
+    wantsMailRestockProduct: null,  
+    error: "",
+    success: "",
+  }),
+  actions: {
+    async login(email, password) {
+      try {
+        const response = await instance.post('auth/login', { email, password });
 
-        async login(email, password) {
-            try {
-                const response = await axios.post('http://localhost:8000/auth/login', {
-                    email: email,
-                    password: password
-                });
+        const userData = response.data.user;
 
-                const userData = response.data.user;
+        this.token = response.data.token;
+        this.nom = userData.nom;
+        this.prenom = userData.prenom;
+        this.id = userData.id;
+        this.email = userData.email;
+        this.role = userData.role;
+        this.wantsMailChangingPrice = userData.wantsMailChangingPrice;
+        this.wantsMailNewProduct = userData.wantsMailNewProduct;
+        this.wantsMailNewsletter = userData.wantsMailNewsletter;
+        this.wantsMailRestockProduct = userData.wantsMailRestockProduct;
 
-                this.token = response.data.token;
-                this.nom = userData.nom;
-                this.prenom = userData.prenom;
-                this.id = userData.id;
-                this.email = userData.email;
-                this.role = userData.role;
-                this.wantsMailChangingPrice = userData.wantsMailChangingPrice;
-                this.wantsMailNewProduct = userData.wantsMailNewProduct;
-                this.wantsMailNewsletter = userData.wantsMailNewsletter;
-                this.wantsMailRestockProduct = userData.wantsMailRestockProduct;
+        instance.defaults.headers.common['Authorization'] = `Bearer ${response.data.token}`;
 
-                this.success = 'Login successful!';
-                this.error = '';
-                
-            } catch (err) {
-                console.error('Error during login:', err);
-                this.error = err.response?.data?.error || 'An error occurred.';
-                this.success = '';
-            }
-        },
-        logout() {
-            console.log('Logging out...');
-            this.token = '';
-            this.nom = '';
-            this.id = '';
-            this.prenom = '';
-            this.email = '';
-            this.role = '';
-            this.success = '';
-            this.error = '';
-        }
+        localStorage.setItem('user', JSON.stringify({
+          token: this.token
+        }));
+
+        this.success = 'Login successful!';
+        this.error = '';
+        
+      } catch (err) {
+        console.error('Error during login:', err);
+        this.error = err.response?.data?.error || 'An error occurred.';
+        this.success = '';
+      }
+    },
+    logout() {
+      console.log('Logging out...');
+      this.token = '';
+      this.nom = '';
+      this.id = '';
+      this.prenom = '';
+      this.email = '';
+      this.role = '';
+      this.success = '';
+      this.error = '';
+
+      localStorage.removeItem('user');
+      instance.defaults.headers.common['Authorization'] = '';
+    },
+    checkToken() {
+      const user = localStorage.getItem('user');
+      if (user) {
+        const parsedUser = JSON.parse(user);
+        this.token = parsedUser.token;
+        instance.defaults.headers.common['Authorization'] = `Bearer ${parsedUser.token}`;
+      }
     }
+  }
 });
