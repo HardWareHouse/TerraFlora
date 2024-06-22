74d00cab70544c3abf6f427ebe802d713ca7a518
AriaAman <ariaaman@outlook.fr>
2024-06-22 16:36:18 +0200

    fix issues

--- server/src/modelsSQL/indexSQL.js
+++ server/src/modelsSQL/indexSQL.js
@@ -1,13 +1,13 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const connection = require("./db");
+const connection = require("./dataBase.js");
 
 const files = fs.readdirSync(__dirname);
 const db = {
   connection,
 };
 for (const file of files) {
-  if (["index.js", "db.js"].includes(file)) continue;
+  if (["index.js", "dataBase.js"].includes(file)) continue;
   const model = require(path.join(__dirname, file))(connection);
   db[model.name] = model;
 }
