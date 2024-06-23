87a3c79a4b8a9c5fa9883fb0c5a573186da633d5
AriaAman <ariaaman@outlook.fr>
2024-06-23 22:26:28 +0200

    Update Front-end et utilisation du tokenMailPreference au lieu du userID lors du changement des preferences des mails (Front)

--- client/src/components/dashboard/wrapperComponents/Dashboard.vue
+++ client/src/components/dashboard/wrapperComponents/Dashboard.vue
@@ -3,7 +3,7 @@
     <h3 class="text-2xl font-medium pb-2 border-b border-gray-200">Tableau de bord</h3>
     <div class="mt-4 text-[14px] text-gray-600">
       <p class="">
-        Salut, <strong class="text-red-600">Oliver Moelleux</strong> !
+        Salut, <strong class="text-red-600">{{ nomPrenom }}</strong> !
       </p>
     </div>
     <p class="mt-1 text-[14px] text-gray-600">
@@ -13,6 +13,12 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import { useAuthStore } from '../../../pinia/auth.js'; 
+
+const authStore = useAuthStore();
+
+const nomPrenom = ref(authStore.nom + ' ' + authStore.prenom);
 </script>
 
 <style scoped>
