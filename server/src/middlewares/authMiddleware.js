e6dae431dc7c0b07708df65eafcbd64fae9fc66a
Traore Moussa Seydou <moussa21sr@gmail.com>
2024-06-29 18:03:32 +0200

    correction de la méthode authenticate du authMiddleware

--- server/src/middlewares/authMiddleware.js
+++ server/src/middlewares/authMiddleware.js
@@ -15,7 +15,7 @@ export const authenticate = async (req, res, next) => {
             return res.status(401).json({ error: 'User not found.' });
         }
 
-        //req.user = user;
+        req.user = user;
         next();
     } catch (err) {
         if (err.name === 'JsonWebTokenError') {
