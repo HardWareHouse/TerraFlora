4de333b668720bf33afd59059f1ea7aed393f8cb
Jay Bury <buryj15@gmail.com>
2024-07-08 20:21:58 +0200

    ajouter stripe a la page checkout

--- client/src/components/basket/checkout.vue
+++ client/src/components/basket/checkout.vue
@@ -11,12 +11,34 @@
           <span>Livraison</span>
           <span>{{ shipping }}€</span>
         </div>
-        <div class="flex justify-between items-center py-2 border-b font-semibold text-lg">
+        <div
+          class="flex justify-between items-center py-2 border-b font-semibold text-lg"
+        >
           <span>Total</span>
           <span class="text-red-600">{{ total }}€</span>
         </div>
-        <RouterLink to="/checkout" class="w-full bg-red-600 text-white py-2 mt-4 rounded">Passer à la caisse
-        </RouterLink>
+        <div>
+          <stripe-checkout
+            ref="checkoutRef"
+            mode="payment"
+            :pk="publishableKey"
+            :line-items="lineItems"
+            :success-url="successURL"
+            :cancel-url="cancelURL"
+            @loading="(v) => (loading = v)"
+          />
+          <button
+            @click="submit"
+            class="w-full bg-red-600 text-white py-2 mt-4 rounded"
+          >
+            Passer à la caisse
+          </button>
+        </div>
+        <!-- <RouterLink
+          to="/stripe"
+          class="w-full bg-red-600 text-white py-2 mt-4 rounded"
+          >Passer à la caisse
+        </RouterLink> -->
       </div>
     </div>
   </div>
@@ -41,5 +63,56 @@ const proceedToCheckout = () => {
   console.log("Proceeding to checkout...");
 };
 </script>
+<script>
+import { StripeCheckout } from "@vue-stripe/vue-stripe";
+import { useCartStore } from "../../pinia/cart.js";
 
+export default {
+  components: {
+    StripeCheckout,
+  },
+  data() {
+    return {
+      loading: false,
+      publishableKey: `${import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY}`,
+      lineItems: [],
+      successURL: `${window.location.origin}/success`,
+      cancelURL: `${window.location.origin}/cancel`,
+    };
+  },
+  computed: {
+    cartItems() {
+      const cartStore = useCartStore();
+      return cartStore.items || [];
+    },
+  },
+  watch: {
+    cartItems: {
+      handler(newItems) {
+        if (Array.isArray(newItems)) {
+          this.updateLineItems(newItems);
+        } else {
+          console.error("cartItems is not an array:", newItems);
+        }
+      },
+      immediate: true,
+    },
+  },
+  methods: {
+    updateLineItems(cartItems) {
+      this.lineItems = cartItems.map((item) => ({
+        price: item.priceId,
+        quantity: item.quantity,
+      }));
+    },
+    submit() {
+      if (this.lineItems.length === 0) {
+        console.error("lineItems is empty, cannot proceed to checkout");
+        return;
+      }
+      this.$refs.checkoutRef.redirectToCheckout();
+    },
+  },
+};
+</script>
 <style scoped></style>
