379e28c5d0af269f408397ab28ec035ad3421c6a
Traore Moussa Seydou <moussa21sr@gmail.com>
2024-07-16 14:44:19 +0200

    Ajout du formulaire de contact côté back

---
+++ server/src/middlewares/contactMiddleware.js
@@ -0,0 +1,47 @@
+import { Op } from 'sequelize';
+import Contact from "../modelsSQL/Contact.js";
+
+export const isDailyLimitContactExceeded = async (req, res, next) => {
+    const user = req.user;
+    const date = new Date();
+    date.setHours(0, 0, 0, 0);
+
+    const contacts = await Contact.findAll({
+        where: {
+            userId: user.id,
+            status: {
+                [Op.ne]: 'traité'
+            },
+            createdAt: {
+                [Op.gte]: date
+            }
+        }
+    });
+    if (contacts.length >= 5) {
+        return res.status(400).json({ error: "Daily limit reached" });
+    }
+    next();
+};
+
+export const isMonthlyLimitContactExceeded = async (req, res, next) => {
+    const user = req.user;
+    const date = new Date();
+    date.setDate(1);
+    date.setHours(0, 0, 0, 0);
+    
+    const contacts = await Contact.findAll({
+        where: {
+            userId: user.id,
+            status: {
+                [Op.ne]: 'traité'
+            },
+            createdAt: {
+                [Op.gte]: date
+            }
+        }
+    });
+    if (contacts.length >= 12) {
+        return res.status(400).json({ error: "Monthly limit reached" });
+    }
+    next();
+};
---
+++ server/src/routes/contact.js
@@ -0,0 +1,21 @@
+import Router from 'express';
+import { 
+    getContact, 
+    getAllContacts, 
+    getContactByUserId, 
+    createContact, 
+    updateContact, 
+    deleteContact 
+} from '../controllers/contactController.js';
+import { authenticate, authorizeAdmin } from '../middlewares/authMiddleware.js';
+import { isDailyLimitContactExceeded, isMonthlyLimitContactExceeded } from '../middlewares/contactMiddleware.js';
+
+const router = Router();
+
+router.get('/', authenticate, authorizeAdmin, getAllContacts);
+router.get('/:id', authenticate, getContact);
+router.post('/', authenticate, isDailyLimitContactExceeded, isMonthlyLimitContactExceeded, createContact);
+router.put('/:id', authenticate, updateContact);
+router.delete('/:id', authenticate, authorizeAdmin, deleteContact);
+
+export default router;
